579acbf0b201b96532b8a33af5d5192bb7dcb510
tommos4ki <sarimu.medfai@gmail.com>
2024-08-15 01:19:30 +0100

    making slash commaands work

--- assets/commands.py
+++ assets/commands.py
@@ -7,16 +7,18 @@ class CommandTree:
         self.tree = bot.tree
 
     def add_commands(self):
-        @self.tree.command(name = "ping", description = "Replies with Pong!")
+        @self.tree.command(name = "test", description = "Replies with Pong!")
         async def ping(interaction: discord.Interaction):
-            await interaction.response.send_message("Pong!")
+            await interaction.response.send_message("test yea!")
 
 
 
 
 
-        @self.tree.command(name = "hello", description = "Says hello!")
-        async def hello(interaction: discord.Interaction):
-            await interaction.response.send_message("Hello!")
+        
+
+
+
+
     async def sync_commands(self):
         await self.tree.sync()
--- main.py
+++ main.py
@@ -7,28 +7,36 @@ from dotenv import load_dotenv
 
 from assets.commands import CommandTree
 
-
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
 
 
 load_dotenv()
 
 activity = discord.Activity(type=discord.ActivityType.listening, name="Hutao AI")
-bot = commands.Bot(command_prefix='!', activity=activity, intents=discord.Intents.all(), status=discord.Status.dnd, appliction_id=1223314662863671326)
+client = commands.Bot(
+    command_prefix='!', 
+    activity=activity, 
+    intents= intents, 
+    status=discord.Status.dnd, 
+    appliction_id=1223314662863671326)
 
 
 
-command_tree = CommandTree(bot)
+command_tree = CommandTree(client)
 command_tree.add_commands()
-asyncio.run(command_tree.sync_commands())
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{client.user.name} has connected to Discord!')
+    print(f"application id: {client.application_id}")
+    await command_tree.sync_commands()
 
 if __name__ == '__main__':
     token = os.getenv('DISCORD_HUTAO_AI_TOKEN')
     try:
-        bot.run(token)
+        client.run(token)
     except Exception as e:
         print(f"ERROR: {e}")
