e84c78c91b59cbc5a54cfc6cba16b161646afa26
tommos4ki <sarimu.medfai@gmail.com>
2024-10-01 13:03:42 +0100

    update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from assets.commands import CommandTree
 from assets.dms import handle_dm
 
-from assets.log import log
+#from assets.log import log
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -37,7 +37,7 @@ command_tree.add_commands()
 
 @client.event
 async def on_ready():
-    print(f'{log()} {client.user.name} has connected to Discord!')
+    print(f' {client.user.name} has connected to Discord!')
     await command_tree.sync_commands()
 
 @client.event
@@ -57,4 +57,4 @@ if __name__ == '__main__':
     try:
         client.run(token)
     except Exception as e:
-        print(f"{log()} ERROR: {e}")
+        print(f" ERROR: {e}")
