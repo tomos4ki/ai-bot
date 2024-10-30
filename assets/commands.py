6dffb576532bc1d036ace6da5d1c530a7b9edc98
tommos4ki <sarimu.medfai@gmail.com>
2024-10-30 15:24:43 +0100

    update 2.1.22 : updating command test with ping with it

--- assets/commands.py
+++ assets/commands.py
@@ -9,15 +9,18 @@ class CommandTree:
     def add_commands(self):
         @self.tree.command(name = "test", description = "replies and shows you the ping between you and the bot's server")
         async def ping(interaction: discord.Interaction):
-            await interaction.response.send_message("test works")
+            await interaction.response.defer()
+
             # Get the ping between the bot and the Discord server
             server_ping = round(self.bot.latency * 1000)
 
             #get the ping between the bot and the server
             user_ping = round((discord.utils.utcnow() - interaction.created_at).total_seconds() * 1000)
 
+            #sending initial  responce
+            await interaction.followup.send("test works")
             #sending the ping result
-            await interaction.response.send_message(f"Bot ping: {server_ping}ms\nUser ping: {user_ping}ms")
+            await interaction.followup.send(f"Bot ping: {server_ping}ms\nUser ping: {user_ping}ms")
 
         
 
@@ -25,17 +28,7 @@ class CommandTree:
         async def ping(interaction: discord.Interaction):
             await interaction.response.send_message("opening winter ark app in your dm")
     
-    def add_commands_winter_ark(self):
-        @self.tree.command(name = "winter_ark", description = "opens winter ark app in your dm")
-        async def ping(interaction: discord.Interaction):
-            await interaction.response.send_message("opening winter ark app in your dm")
-
-
-
-
-
-        
-
+    
 
 
 
