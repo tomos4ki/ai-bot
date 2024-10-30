3c374cae424796f66e20bd7a990a90a3253fbd1f
tommos4ki <sarimu.medfai@gmail.com>
2024-10-30 12:26:59 +0100

    update 2.1.21 : updating test and adding ping tests(testing it)

--- assets/commands.py
+++ assets/commands.py
@@ -7,9 +7,17 @@ class CommandTree:
         self.tree = bot.tree
 
     def add_commands(self):
-        @self.tree.command(name = "test", description = "Replies with Pong!")
+        @self.tree.command(name = "test", description = "replies and shows you the ping between you and the bot's server")
         async def ping(interaction: discord.Interaction):
-            await interaction.response.send_message("test yea!")
+            await interaction.response.send_message("test works")
+            # Get the ping between the bot and the Discord server
+            server_ping = round(self.bot.latency * 1000)
+
+            #get the ping between the bot and the server
+            user_ping = round((discord.utils.utcnow() - interaction.created_at).total_seconds() * 1000)
+
+            #sending the ping result
+            await interaction.response.send_message(f"Bot ping: {server_ping}ms\nUser ping: {user_ping}ms")
 
         
 
